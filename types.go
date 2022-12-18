0fa7dfdfd37daf525e91e661b31cec77bd31e0e0
Logan Kennelly <logan@logank.dev>
2022-12-17 21:21:14 -0800

    types: Add Song

--- types.go
+++ types.go
@@ -2,6 +2,7 @@ package ampache
 
 import (
 	"encoding/xml"
+	"net/url"
 	"time"
 )
 
@@ -18,4 +19,90 @@ func (c *xmlTime) UnmarshalXML(d *xml.Decoder, start xml.StartElement) error {
 	}
 	*c = xmlTime{parse}
 	return nil
+}
+
+type xmlDuration struct {
+	time.Duration
+}
+
+func (c *xmlDuration) UnmarshalXML(d *xml.Decoder, start xml.StartElement) error {
+	var v int
+	if err := d.DecodeElement(&v, &start); err != nil {
+		return err
+	}
+	c.Duration = time.Duration(v) * time.Second
+	return nil
+}
+
+type xmlURL struct {
+	url.URL
+}
+
+func (c *xmlURL) UnmarshalXML(d *xml.Decoder, start xml.StartElement) error {
+	var v string
+	if err := d.DecodeElement(&v, &start); err != nil {
+		return err
+	}
+	u, err := url.Parse(v)
+	if err != nil {
+		return err
+	}
+	c.URL = *u
+	return nil
+}
+
+type Artist struct {
+	Id   int    `xml:"id,attr"`
+	Name string `xml:",chardata"`
+}
+
+type Album struct {
+	Id   int    `xml:"id,attr"`
+	Name string `xml:",chardata"`
+}
+
+type Song struct {
+	Id              int         `xml:"id,attr"`
+	Title           string      `xml:"title"`
+	Name            string      `xml:"name"`
+	Artist          Artist      `xml:"artist"`
+	Album           Album       `xml:"album"`
+	AlbumArtist     Artist      `xml:"albumartist"`
+	Disk            int         `xml:"disk"`
+	Track           int         `xml:"track"`
+	Genre           string      `xml:"genre"`
+	Filename        string      `xml:"filename"`
+	PlaylistTrack   int         `xml:"playlisttrack"`
+	Time            xmlDuration `xml:"time"`
+	Year            int         `xml:"year"`
+	BitRate         int         `xml:"bitrate"`
+	Rate            int         `xml:"rate"`
+	Mode            string      `xml:"mode"`
+	Mime            string      `xml:"mime"`
+	Url             xmlURL      `xml:"url"`
+	Size            int         `xml:"size"`
+	Mbid            string      `xml:"mbid"`
+	AlbumMbid       string      `xml:"album_mbid"`
+	ArtistMbid      string      `xml:"artist_mbid"`
+	AlbumArtistMbid string      `xml:"albumartist_mbid"`
+	Art             xmlURL      `xml:"art"`
+	Flag            int         `xml:"flag"`
+	PreciseRating   int         `xml:"preciserating"`
+	Rating          int         `xml:"rating"`
+	AverageRating   string      `xml:"averagerating"`
+	PlayCount       int         `xml:"playcount"`
+	Catalog         int         `xml:"catalog"`
+	Composer        string      `xml:"composer"`
+	Channels        int         `xml:"channels"`
+	Comment         string      `xml:"comment"`
+	License         string      `xml:"license"`
+	Publisher       string      `xml:"publisher"`
+	Language        string      `xml:"language"`
+	Lyrics          string      `xml:"lyrics"`
+	AlbumGain       float64     `xml:"replaygain_album_gain"`
+	AlbumPeak       float64     `xml:"replaygain_album_peak"`
+	TrackGain       float64     `xml:"replaygain_track_gain"`
+	TrackPeak       float64     `xml:"replaygain_track_peak"`
+	R128AlbumGain   int         `xml:"r128_album_gain"`
+	R128TrackGain   int         `xml:"r128_track_gain"`
 }
--- types_test.go
+++ types_test.go
@@ -2,6 +2,7 @@ package ampache_test
 
 import (
 	"encoding/xml"
+	"net/url"
 	"testing"
 	"time"
 
@@ -54,5 +55,73 @@ func TestHandshake(t *testing.T) {
 	wantSongs := 75
 	if v.Songs != wantSongs {
 		t.Errorf("Unexpected <songs> = %d; want %d", v.Songs, wantSongs)
+	}
+}
+
+func TestSong(t *testing.T) {
+	raw := `<?xml version="1.0" encoding="UTF-8"?>
+<song id="8195">
+  <title><![CDATA[Duality]]></title>
+  <name><![CDATA[Duality]]></name>
+  <artist id="400"><![CDATA[Bayside]]></artist>
+  <album id="691"><![CDATA[The Walking Wounded]]></album>
+  <albumartist id="0"><![CDATA[]]></albumartist>
+  <disk><![CDATA[1]]></disk>
+  <track>3</track>
+  <genre id="36"><![CDATA[Alternative & Punk]]></genre>
+  <filename><![CDATA[/media/iTunes/iTunes Music/Bayside/The Walking Wounded/03 Duality.m4a]]></filename>
+  <playlisttrack>2</playlisttrack>
+  <time>180</time>
+  <year>2007</year>
+  <bitrate>162837</bitrate>
+  <rate>44100</rate>
+  <mode><![CDATA[vbr]]></mode>
+  <mime><![CDATA[audio/mp4]]></mime>
+  <url><![CDATA[https://example.com/play/index.php?ssid=35e1bb81dc07bc36862a9a042b470b67&type=song&oid=8195&uid=2&player=api&name=Bayside%20-%20Duality.m4a]]></url>
+  <size>3707354</size>
+  <mbid><![CDATA[]]></mbid>
+  <album_mbid><![CDATA[]]></album_mbid>
+  <artist_mbid><![CDATA[]]></artist_mbid>
+  <albumartist_mbid><![CDATA[]]></albumartist_mbid>
+  <art><![CDATA[https://example.com/image.php?object_id=691&object_type=album&auth=35e1bb81dc07bc36862a9a042b470b67]]></art>
+  <flag>0</flag>
+  <preciserating/>
+  <rating>5</rating>
+  <averagerating/>
+  <playcount>0</playcount>
+  <catalog>1</catalog>
+  <composer><![CDATA[Bayside]]></composer>
+  <channels/>
+  <comment><![CDATA[]]></comment>
+  <license><![CDATA[]]></license>
+  <publisher><![CDATA[]]></publisher>
+  <language/>
+  <lyrics><![CDATA[]]></lyrics>
+  <replaygain_album_gain/>
+  <replaygain_album_peak/>
+  <replaygain_track_gain/>
+  <replaygain_track_peak/>
+  <r128_album_gain/>
+  <r128_track_gain/>
+</song>`
+
+	var v ampache.Song
+	if err := xml.Unmarshal([]byte(raw), &v); err != nil {
+		t.Fatalf("Unmarshal failed for Song: %s input =\n%s", err, raw)
+	}
+
+	wantId := 8195
+	if v.Id != wantId {
+		t.Errorf("Unexpected <song id> = %d; want %d", v.Id, wantId)
+	}
+
+	wantTime := time.Duration(180 * time.Second)
+	if v.Time.Duration != wantTime {
+		t.Errorf("Unexpected <time> = %d; want %d", v.Time, wantTime)
+	}
+
+	wantArt, _ := url.Parse("https://example.com/image.php?object_id=691&object_type=album&auth=35e1bb81dc07bc36862a9a042b470b67")
+	if v.Art.URL.String() != wantArt.String() {
+		t.Errorf("Unexpected <art> = '%s'; want '%s'", v.Art.URL.String(), wantArt)
 	}
 }

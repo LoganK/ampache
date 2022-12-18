62239085055ef67d5eefc0b9be77870253ef5f5a
Logan Kennelly <logan@logank.dev>
2022-12-17 22:48:01 -0800

    xmlTime: Fix error in time format

--- types.go
+++ types.go
@@ -13,7 +13,7 @@ type xmlTime struct {
 func (c *xmlTime) UnmarshalXML(d *xml.Decoder, start xml.StartElement) error {
 	var v string
 	d.DecodeElement(&v, &start)
-	parse, err := time.Parse("2006-01-02T03:04:05-07:00", v)
+	parse, err := time.Parse("2006-01-02T15:04:05-07:00", v)
 	if err != nil {
 		return err
 	}
